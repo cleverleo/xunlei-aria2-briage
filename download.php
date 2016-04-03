a2d111beaf438d143bebac0f87b04aa5b9ffcce9
leoliang <liangxiaopeng@gmail.com>
2016-04-03 17:29:20 +0800

    下载目录错误

--- download.php
+++ download.php
@@ -21,20 +21,17 @@ if (!isset($_POST['url'])) {
 }
 
 $aria2 = new Aria2('http://127.0.0.1:6800/jsonrpc');
-$glb_option = $aria2->getGlobalOption();
 
 if (!isset($glb_option['result']['dir'])) {
     throw new Exception('The Aria2 Server Not Correct');
 }
-
-$base_dir = $glb_option['result']['dir'];
 
 $gdriveid = XunleiLixian::getInfo()['gdriveid'];
 $file_list = XunleiLixian::downloadByUrl($_POST['url']);
 
 
 foreach ($file_list as $item) {
-    $aria2->addUri([$item['download_link']], ['header' => ['Cookie: gdriveid=' . $gdriveid], 'out' => $base_dir . $item['file_path']]);
+    $aria2->addUri([$item['download_link']], ['header' => ['Cookie: gdriveid=' . $gdriveid], 'out' => $item['file_path']]);
 }
 
 die(json_encode(['success' => true, 'file_count' => count($file_list)]));
